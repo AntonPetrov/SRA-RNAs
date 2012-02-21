071137a2860e41da4008cd1fe3e7eaac14bab7e6
Anton Petrov <anton.i.petrov@gmail.com>
2012-02-21 11:29:26 -0500

    Switch links from Ty1 to SRA

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -1,18 +1,11 @@
     <div class="topbar" data-dropdown="dropdown">
       <div class="fill">
         <div class="container">
-          <a class="brand" href="<?php echo $baseurl;?>">Ty1 Analysis</a>
+          <a class="brand" href="<?php echo $baseurl;?>">SRA Analysis</a>
           <ul class="nav">
             <li><a href="http://rna.bgsu.edu/research/jar3d" target="_blank">JAR3D</a></li>
-            <li><a href="http://goo.gl/aYyy4" target="_blank">Google Doc</a></li>
-            <li><a href="https://github.com/AntonPetrov/Ty1-RNAs" target="_blank">Github</a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle">Env</a>
-                <ul class="dropdown-menu">
-                    <li><a href="http://rna.bgsu.edu/research/ty1">Production</a></li>
-                    <li><a href="http://rna.bgsu.edu/research/ty1_dev">Dev</a></li>
-                </ul>
-            </li>
+            <li><a href="http://goo.gl/REYKX" target="_blank">Google Doc</a></li>
+            <li><a href="https://github.com/AntonPetrov/SRA-RNAs" target="_blank">Github</a></li>
           </ul>
         </div>
       </div>
